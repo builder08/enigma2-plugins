89c4504a657e7bc6bdf90a374bceb8091fdaf786
builder08 <75539906+builder08@users.noreply.github.com>
2020-12-06 01:22:47 +0200

    Add files via upload

--- epgsearch/src/__init__.py
+++ epgsearch/src/__init__.py
@@ -1,188 +1,42 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
 from Components.Language import language
-from Components.NimManager import nimmanager
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-from boxbranding import getImageDistro
-from enigma import eServiceReference, eServiceCenter
-import os, gettext
+from os import environ as os_environ
+import gettext
 
 # Config
-from Components.config import config, configfile, ConfigSet, ConfigSubsection, ConfigSelection, ConfigSelectionNumber, ConfigYesNo, ConfigSatlist
-
-import six
-from six.moves import range
-
-PluginLanguageDomain = "EPGSearch"
-PluginLanguagePath = "Extensions/EPGSearch/locale"
+from Components.config import config, ConfigSet, ConfigSubsection, ConfigText, ConfigNumber, ConfigYesNo, ConfigSelection
 
 def localeInit():
-	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+	gettext.bindtextdomain("EPGSearch", resolveFilename(SCOPE_PLUGINS, "Extensions/EPGSearch/locale"))
 
 def _(txt):
-	if gettext.dgettext(PluginLanguageDomain, txt):
-		return gettext.dgettext(PluginLanguageDomain, txt)
-	else:
-		print("[" + PluginLanguageDomain + "] fallback to default translation for " + txt)
-		return gettext.gettext(txt)
+	t = gettext.dgettext("EPGSearch", txt)
+	if t == txt:
+		t = gettext.gettext(txt)
+	return t
 
-language.addCallback(localeInit())
+# Language
+localeInit()
+language.addCallback(localeInit)
+
 
 config.plugins.epgsearch = ConfigSubsection()
-config.plugins.epgsearch.showinplugins = ConfigYesNo(default = False)
-__searchDefaultScope = "currentbouquet" if getImageDistro() in ("easy-gui-aus", "beyonwiz") else "all"
-config.plugins.epgsearch.scope = ConfigSelection(choices=[("all", _("all services")), ("allbouquets", _("all bouquets")), ("currentbouquet", _("current bouquet")), ("currentservice", _("current service")), ("ask", _("ask user"))], default=__searchDefaultScope)
-config.plugins.epgsearch.defaultscope = ConfigSelection(choices=[("all", _("all services")), ("allbouquets", _("all bouquets")), ("currentbouquet", _("current bouquet")), ("currentservice", _("current service"))], default=__searchDefaultScope)
-config.plugins.epgsearch.search_type = ConfigSelection(default = "partial", choices = [("partial", _("partial match")), ("exact", _("exact match")), ("start", _("title starts with"))])
-config.plugins.epgsearch.search_case = ConfigSelection(default = "insensitive", choices = [("insensitive", _("case-insensitive search")), ("sensitive", _("case-sensitive search"))])
-allowShowOrbital = getImageDistro() not in ("easy-gui-aus", "beyonwiz")
-config.plugins.epgsearch.showorbital = ConfigYesNo(default = allowShowOrbital)
 config.plugins.epgsearch.history = ConfigSet(choices = [])
-# XXX: configtext is more flexible but we cannot use this for a (not yet created) gui config
-config.plugins.epgsearch.encoding = ConfigSelection(choices = ['UTF-8', 'ISO8859-15'], default = 'UTF-8')
-config.plugins.epgsearch.history_length = ConfigSelectionNumber(0, 50, 1, default = 10)
+config.plugins.epgsearch.encoding = ConfigSelection([('UTF-8', 'UTF-8'),('ISO8859-15', 'ISO8859-15')], default='UTF-8')
+config.plugins.epgsearch.history_length = ConfigNumber(default = 10)
 config.plugins.epgsearch.add_search_to_epg = ConfigYesNo(default = True)
+config.plugins.epgsearch.type_button_blue = ConfigSelection([('0', _("only Search")),('1', _("choice (Search and standard)"))], default='0')
+config.plugins.epgsearch.picons = ConfigYesNo(default = False)
+config.plugins.epgsearch.bouquet = ConfigYesNo(default = False)
+config.plugins.epgsearch.favorit_name = ConfigYesNo(default = False)
+config.plugins.epgsearch.show_in_furtheroptionsmenu = ConfigYesNo(default = True)
+config.plugins.epgsearch.search_in_channelmenu = ConfigYesNo(default = True)
+config.plugins.epgsearch.filter_type = ConfigSelection(default = "exact", choices = [("partial", _("partial match")), ("exact", _("exact match"))])
+config.plugins.epgsearch.search_case = ConfigSelection(default = "insensitive", choices = [("insensitive", _("case-insensitive search")), ("sensitive", _("case-sensitive search"))])
+config.plugins.epgsearch.search_type = ConfigSelection(default = "partial", choices = [("partial", _("partial match")), ("exact", _("exact match")), ("start", _("title starts with"))])
 
-orbposDisabled = 3600
 
-def getNamespaces(namespaces):
-	lamedbServices = eServiceReference("1:7:1:0:0:0:0:0:0:0:" + " || ".join(['(satellitePosition == %d)' % (x >> 16) for x in namespaces]))
-	hasNamespaces = set()
-	if not namespaces:
-		return hasNamespaces
-	serviceHandler = eServiceCenter.getInstance()
-	servicelist = serviceHandler.list(lamedbServices)
-	if servicelist is not None:
-		serviceIterator = servicelist.getNext()
-		while serviceIterator.valid():
-			ns = serviceIterator.getUnsignedData(4)
-			if ns not in hasNamespaces and ns in namespaces:
-				hasNamespaces.add(ns)
-				if len(hasNamespaces) == len(namespaces):
-					return hasNamespaces
-			serviceIterator = servicelist.getNext()
-	return hasNamespaces
 
-def orbposChoicelist():
-	choiceList = [(orbposDisabled, _('disabled'), 0)]
-	nsDVBT = 0xeeee << 16
-	nsDVBC = 0xffff << 16
-	namespaces = set()
-	if nimmanager.hasNimType("DVB-T") and nimmanager.terrestrialsList:
-		namespaces.add(nsDVBT)
-	if nimmanager.hasNimType("DVB-C") and nimmanager.cablesList:
-		namespaces.add(nsDVBC)
-	hasNamespaces = getNamespaces(namespaces)
-	if nsDVBT in hasNamespaces:
-		choiceList.append((nsDVBT >> 16, _('DVB-T Terrestrial services'), 0))
-	if nsDVBC in hasNamespaces:
-		choiceList.append((nsDVBC >> 16, _('DVB-C Cable services'), 0))
-	choiceList += [
-		(orbpos, nimmanager.getSatDescription(orbpos), 0)
-		for orbpos in sorted(
-			nimmanager.getConfiguredSats(),
-			key=lambda x: x if x <= 1800 else x - 3600
-		)
-	]
-	return choiceList
 
-def isOrbposName(name):
-	return name.startswith("orbpos") and name[6:].isdigit()
 
-def doSave():
-	saveFile = False
-	for name, confItem in six.iteritems(config.plugins.epgsearch.dict()):
-		if (name == "numorbpos" or isOrbposName(name)) and confItem.isChanged():
-			saveFile = True
-			confItem.save()
-	if saveFile:
-		configfile.save()
-
-def unusedOrbPosConfList():
-	numorbpos = int(config.plugins.epgsearch.numorbpos.value)
-	return [
-		item for item in six.iteritems(config.plugins.epgsearch.dict())
-		if isOrbposName(item[0]) and int(item[0][6:]) >= numorbpos
-	]
-
-def initOrbposConfigs():
-	choiceList = orbposChoicelist()
-	maxEntries = len(choiceList)
-	oldVal = config.plugins.epgsearch.getSavedValue().get('numorbpos')
-	listShrank = oldVal is not None and int(oldVal) > maxEntries
-	choices = [str(i) for i in range(maxEntries + 1)]
-	config.plugins.epgsearch.numorbpos = ConfigSelection(choices, default="1")
-	if listShrank:
-		config.plugins.epgsearch.numorbpos.value = str(maxEntries)
-
-	# If the list got smaller, try to preserve the values of as
-	# many entries as possible in the new list.
-	orbPosList = []
-	for name in (name for name in list(config.plugins.epgsearch.getSavedValue().keys()) if isOrbposName(name)):
-		setattr(config.plugins.epgsearch, name, ConfigSatlist(choiceList, default=orbposDisabled))
-		if listShrank:
-			orbPosList.append((getattr(config.plugins.epgsearch, name), int(name[6:])))
-	if orbPosList:
-		orbPosList.sort(key=lambda x: x[1])
-		orbPosList.sort(key=lambda x: int(x[0].value) == orbposDisabled)
-		for i in range(min(maxEntries, len(orbPosList))):
-			getattr(config.plugins.epgsearch, "orbpos" + str(i)).value = orbPosList[i][0].value
-
-def updateOrbposConfig(save=False):
-	choiceList = orbposChoicelist()
-	updateNumOrbpos(choiceList, save)
-	setChoiceList = None
-	# Add any new items & update any existing ones
-	for filt in range(int(config.plugins.epgsearch.numorbpos.value)):
-		name = "orbpos" + str(filt)
-		if hasattr(config.plugins.epgsearch, name):
-			if setChoiceList is None:
-				setChoiceList = [(str(orbpos), desc) for (orbpos, desc, flags) in choiceList]
-			confItem = getattr(config.plugins.epgsearch, name)
-			confItem.setChoices(setChoiceList, default=str(orbposDisabled))
-		else:
-			setattr(config.plugins.epgsearch, name, ConfigSatlist(choiceList, default=orbposDisabled))
-	if save:
-		doSave()
-	return len(choiceList)
-
-# Set old items to default so that they will disappear from the settings file
-def purgeOrbposConfig():
-	for name, confItem in unusedOrbPosConfList():
-		if confItem.value != confItem.default:
-			confItem.value = confItem.default
-	doSave()
-
-def updateUnusedOrbposConfig(choiceList, save):
-	setChoiceList = [(str(orbpos), desc) for (orbpos, desc, flags) in choiceList]
-	for name, confItem in unusedOrbPosConfList():
-		confItem.setChoices(setChoiceList, default=str(orbposDisabled))
-	if save:
-		doSave()
-
-def getOrbposConfList(includeDisabled=False):
-	return [
-		getattr(config.plugins.epgsearch, "orbpos" + str(filt))
-		for filt in range(int(config.plugins.epgsearch.numorbpos.value))
-		if (
-			includeDisabled or
-			getattr(config.plugins.epgsearch, "orbpos" + str(filt)).orbital_position != orbposDisabled
-		)
-	]
-
-def updateNumOrbpos(choiceList, save):
-	maxEntries = max(len(choiceList) - 1, 0)
-	oldVal = int(config.plugins.epgsearch.numorbpos.value)
-	choices = [str(i) for i in range(maxEntries + 1)]
-	oldMax = len(config.plugins.epgsearch.numorbpos.choices) - 1
-	if oldVal > maxEntries:
-		config.plugins.epgsearch.numorbpos.value = str(maxEntries)
-	config.plugins.epgsearch.numorbpos.setChoices(choices, default="1")
-	updateUnusedOrbposConfig(choiceList, save)
-
-initOrbposConfigs()
-updateOrbposConfig(save=True)
-purgeOrbposConfig()
-
-config.plugins.epgsearch.enableorbpos = ConfigYesNo(default=False)
-config.plugins.epgsearch.invertorbpos = ConfigSelection(choices=[_("include"), _("exclude")], default=_("include"))
--- epgsearch/src/plugin.py
+++ epgsearch/src/plugin.py
@@ -1,98 +1,158 @@
-from __future__ import absolute_import
-# for localized messages
+# for localized messages  
 from . import _
-
 from enigma import eServiceCenter
-
-# Configuration
+from Screens.EpgSelection import EPGSelection
+from Screens.VirtualKeyBoard import VirtualKeyBoard, VKB_SEARCH_ICON
+from Components.EpgList import EPG_TYPE_SINGLE, EPG_TYPE_MULTI
+from Components.ActionMap import ActionMap
+from EPGSearch import EPGSearch, EPGSearchEPGSelection
+from Plugins.Plugin import PluginDescriptor
+from Screens.ChoiceBox import ChoiceBox
 from Components.config import config
 
-# Plugin
-from .EPGSearch import EPGSearch, EPGSearchEPGSelection
+# Overwrite EPGSelection.__init__ with our modified one
+baseEPGSelection__init__ = None
+def EPGSelectionInit():
+	global baseEPGSelection__init__
+	if baseEPGSelection__init__ is None:
+		baseEPGSelection__init__ = EPGSelection.__init__
+	EPGSelection.__init__ = EPGSearchSelection__init__
+	EPGSelection.CallbackChoiceAction = CallbackChoiceAction
+	EPGSelection.VKaction = VKaction
 
-# Plugin definition
-from Plugins.Plugin import PluginDescriptor
+# Modified EPGSelection __init__
+def EPGSearchSelection__init__(self, session, service, zapFunc=None, eventid=None, bouquetChangeCB=None, serviceChangeCB=None, parent=None):
+	baseEPGSelection__init__(self, session, service, zapFunc, eventid, bouquetChangeCB, serviceChangeCB, parent)
+	if self.type != EPG_TYPE_MULTI and config.plugins.epgsearch.add_search_to_epg.value:
+		def bluePressed():
+			if config.plugins.epgsearch.type_button_blue.value == "0":
+				cur = self["list"].getCurrent()
+				if cur[0] is not None:
+					name = cur[0].getEventName()
+				else:
+					name = ''
+				self.session.open(EPGSearch, name, False)
+			elif config.plugins.epgsearch.type_button_blue.value == "1":
+				list = [
+				(_("Search"), "search"),
+				(_("Select channel"), "standard"),
+				(_("Custom search"), "custom"),
+				]
+				dlg = self.session.openWithCallback(self.CallbackChoiceAction, ChoiceBox, title= _("Select action:"), list = list)
+				dlg.setTitle(_("Choice list EPGSearch"))
+		self["epgsearch_actions"] = ActionMap(["EPGSelectActions"],
+				{
+					"blue": bluePressed,
+				})
+		if config.plugins.epgsearch.type_button_blue.value == "0":
+			self["key_blue"].text = _("Search")
+		elif config.plugins.epgsearch.type_button_blue.value == "1":
+			self["key_blue"].text = _("Choice list")
+
+def CallbackChoiceAction(self, ret):
+	ret = ret and ret[1]
+	if ret:
+		if ret == "search" or ret == "custom":
+			try:
+				cur = self["list"].getCurrent()
+				if cur[0] is not None:
+					name = cur[0].getEventName()
+				else:
+					name = ''
+				if ret == "custom":
+					self.session.openWithCallback(self.VKaction, VirtualKeyBoard, text=name, title=_("Enter text to search for"), style=VKB_SEARCH_ICON)
+				else:
+					self.session.open(EPGSearch, name, False)
+			except:
+				pass
+		elif ret == "standard":
+			try:
+				self.blueButtonPressed()
+			except:
+				pass
+
+def VKaction(self, answer):
+	if answer:
+		self.session.open(EPGSearch, answer, False)
+
+# Autostart
+def autostart(reason, **kwargs):
+	try:
+		# for blue key activating in EPGSelection
+		EPGSelectionInit()
+	except Exception:
+		pass
 
 # Mainfunction
 def main(session, *args, **kwargs):
 	s = session.nav.getCurrentService()
 	if s:
 		info = s.info()
-		event = info.getEvent(0) # 0 = now, 1 = next
+		event = info and info.getEvent(0) # 0 = now, 1 = next
 		name = event and event.getEventName() or ''
-	else:
-		name = self.session.nav.getCurrentlyPlayingServiceReference().toString()
-		name = name.split('/')
-		name = name[-1]
-		name = name.replace('.', ' ')
-		name = name.split('-')
-		name = name[0]
-		if name.endswith(' '):
-			name = name[:-1]
-	if name:
 		session.open(EPGSearch, name, False)
 	else:
 		session.open(EPGSearch)
 
+# Channel context menu
+def channelscontext(session, service=None, **kwargs):
+	serviceHandler = eServiceCenter.getInstance()
+	info = serviceHandler.info(service)
+	event = info.getEvent(service)
+	if event:
+		name = info and event.getEventName() or ''
+		session.open(EPGSearch, name)
+
 # Event Info
 def eventinfo(session, eventName="", **kwargs):
-	if not eventName:
-		s = session.nav.getCurrentService()
-		if s:
-			info = s.info()
-			event = info.getEvent(0) # 0 = now, 1 = next
-			eventName = event and event.getEventName() or ''
-	session.open(EPGSearch, eventName)
+	if eventName != "":
+		session.open(EPGSearch, eventName, False)
+	else:
+		ref = session.nav.getCurrentlyPlayingServiceReference()
+		if ref:
+			session.open(EPGSearchEPGSelection, ref, True)
 
-def seachhistory(session, *args, **kwargs):
-	session.open(EPGSearch, openHistory=True)
+# EPG Further Options
+def epgfurther(session, selectedevent, **kwargs):
+	session.open(EPGSearch, selectedevent[0].getEventName())
 
 # Movielist
 def movielist(session, service, **kwargs):
 	serviceHandler = eServiceCenter.getInstance()
 	info = serviceHandler.info(service)
 	name = info and info.getName(service) or ''
+	name = name.split(".")[0].strip()
 	session.open(EPGSearch, name)
 
-pluginlist = PluginDescriptor(name = _("EPGSearch"), description = _("Search EPG"), where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main, needsRestart = False)
-
 def Plugins(**kwargs):
-	l = [
+	path = [
 		PluginDescriptor(
-			# TRANSLATORS: EPGSearch title in EventInfo dialog (does not require further user interaction)
-			# TRANSLATORS: %s inserts a non-printing character and is only used to control the sort order of these WHERE_EVENTINFO entries in Screens.ButtonSetup. Please leave it at the start of the translation string.
-			name = _("%ssearch EPG") % "\x86",
+			where = [PluginDescriptor.WHERE_AUTOSTART, PluginDescriptor.WHERE_SESSIONSTART],
+			fnc = autostart,
+		),
+		PluginDescriptor(
+			name = _("EPGSearch"),
+			description = _("Search EPG by title event"),
+			where = PluginDescriptor.WHERE_PLUGINMENU,
+			fnc = main,
+			icon = "epg.png",
+			needsRestart = False,
+		),
+		PluginDescriptor(
+			name = _("search EPG..."),
 			where = PluginDescriptor.WHERE_EVENTINFO,
 			fnc = eventinfo,
 			needsRestart = False,
 		),
 		PluginDescriptor(
-			# TRANSLATORS: EPGSearch search from search history in EventInfo dialog (requires the user to select a history item to search for)
-			# TRANSLATORS: %s inserts a non-printing character and is only used to control the sort order of these WHERE_EVENTINFO entries in Screens.ButtonSetup. Please leave it at the start of the translation string.
-			name = _("%ssearch EPG from history...") % "\x87",
-			where = PluginDescriptor.WHERE_EVENTINFO,
-			fnc = seachhistory,
-			needsRestart = False,
-		),
-		PluginDescriptor(
-			# TRANSLATORS: EPGSearch title in MovieList (does not require further user interaction)
-			name = _("search EPG"),
 			description = _("search EPG"),
 			where = PluginDescriptor.WHERE_MOVIELIST,
 			fnc = movielist,
 			needsRestart = False,
 		),
-		PluginDescriptor(
-			# TRANSLATORS: EPGSearch search from search history in MovieList (requires the user to select a history item to search for)
-			name = _("search EPG from history..."),
-			description = _("search EPG from history..."),
-			where = PluginDescriptor.WHERE_MOVIELIST,
-			fnc = seachhistory,
-			needsRestart = False,
-		),
 	]
-
-	if config.plugins.epgsearch.showinplugins.value:
-		l.append(pluginlist)
-
-	return l
+	if config.plugins.epgsearch.search_in_channelmenu.value:
+		path.append(PluginDescriptor(name = _("Search event in EPG"), where=PluginDescriptor.WHERE_CHANNEL_CONTEXT_MENU, needsRestart = False, fnc=channelscontext))
+	if config.plugins.epgsearch.show_in_furtheroptionsmenu.value:
+		path.append(PluginDescriptor(name = _("Search event in EPG"), where = PluginDescriptor.WHERE_EVENTINFO, fnc = epgfurther, needsRestart = False))
+	return path
